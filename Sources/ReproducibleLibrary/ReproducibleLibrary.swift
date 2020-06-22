a5022d9d299b0d97a8636191845839fb1ee07c16
Daniel Gstoehl <gstoehl@ubique.ch>
2020-06-22 10:31:36 +0200

    add a class

--- Sources/ReproducibleLibrary/ReproducibleLibrary.swift
+++ Sources/ReproducibleLibrary/ReproducibleLibrary.swift
@@ -1,3 +1,9 @@
 public func hello(message: String) -> String{
     return "Hello " + message
 }
+
+public class ReproLib{
+    public func hello(message: String) -> String{
+        return "Class says hello " + message
+    }
+}
