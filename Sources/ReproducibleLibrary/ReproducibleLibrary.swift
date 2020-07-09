cc25209b4f5731020a5cbf5c8680d07ea9e421bc
Daniel Gstoehl <gstoehl@ubique.ch>
2020-07-09 10:27:30 +0200

    things

--- Sources/ReproducibleLibrary/ReproducibleLibrary.swift
+++ Sources/ReproducibleLibrary/ReproducibleLibrary.swift
@@ -1,14 +1,27 @@
+import SwiftUI
+
 public func hello(message: String) -> String{
     return "Hello " + message
 }
 
 public class ReproLib{
-    let greeting: String
+    var greeting: String
     public init(_greeting: String){
         greeting = _greeting
     }
     
     public func hello(message: String) -> String{
         return greeting + message
+    }
+    
+    public func helloText(message: String)->View{
+        return Text(message)
+    }
+    
+}
+
+public class ReproLib2: ReproLib{
+    public override init(_greeting: String){
+        greeting = _greeting + "!!!"
     }
 }
